8c09036ccf32f333137a11ff2ec920c02f4f6273
Sanjayvel19 <108643891+Sanjayvel19@users.noreply.github.com>
2026-04-06 07:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1148,4 +1148,5 @@ if __name__ == "__main__":
     log.info("=" * 80)
     log.info("")
     
-    app.run(debug=debug_mode, port=5000, host="0.0.0.0", use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+app.run(debug=debug_mode, port=port, host="0.0.0.0", use_reloader=False)
